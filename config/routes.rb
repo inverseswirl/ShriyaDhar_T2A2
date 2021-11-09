fe52c56856072299090587bbf207a21bf2d7e9b8
inverseswirl <68209878+inverseswirl@users.noreply.github.com>
2021-11-09 14:44:05 +1100

    view changes and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'medical_products/index', to: 'medical_products#index' 
+  # get '/medical_products', to: 'medical_products#index'
+  # get '/medical_products/new',  to: 'medical_products#new'
+  # post '/'
+  resources :medical_products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  root 'medical_products#index'
 end
